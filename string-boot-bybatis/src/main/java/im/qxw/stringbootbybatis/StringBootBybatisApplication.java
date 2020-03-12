46764c241a3eaaeaa0134e411b6790902a1fc9e9
Qiao <qxwxintu@gmail.com>
2020-03-13 03:15:53 +0800

    1

---
+++ string-boot-bybatis/src/main/java/im/qxw/stringbootbybatis/StringBootBybatisApplication.java
@@ -0,0 +1,13 @@
+package im.qxw.stringbootbybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringBootBybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StringBootBybatisApplication.class, args);
+    }
+
+}
---
+++ string-boot-bybatis/src/test/java/im/qxw/stringbootbybatis/StringBootBybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package im.qxw.stringbootbybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringBootBybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
