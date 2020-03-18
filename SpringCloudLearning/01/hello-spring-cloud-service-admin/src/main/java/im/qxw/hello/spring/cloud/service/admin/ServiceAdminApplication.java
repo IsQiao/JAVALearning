f1f22277bf7676c52bebcd6a2d2cf7acd13366b3
Qiao <qxwxintu@gmail.com>
2020-03-19 00:31:00 +0800

    service register and discovery

---
+++ SpringCloudLearning/01/hello-spring-cloud-service-admin/src/main/java/im/qxw/hello/spring/cloud/service/admin/ServiceAdminApplication.java
@@ -0,0 +1,13 @@
+package im.qxw.hello.spring.cloud.service.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ServiceAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceAdminApplication.class, args);
+    }
+}
