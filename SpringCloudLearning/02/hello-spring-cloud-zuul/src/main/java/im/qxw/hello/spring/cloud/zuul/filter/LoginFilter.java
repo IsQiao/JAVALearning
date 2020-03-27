584aa6e33486ea8b4452b098357d1909b0bc7733
Qiao <qxwxintu@gmail.com>
2020-03-28 01:52:40 +0800

    zuul api gateway filter

---
+++ SpringCloudLearning/02/hello-spring-cloud-zuul/src/main/java/im/qxw/hello/spring/cloud/zuul/filter/LoginFilter.java
@@ -0,0 +1,60 @@
+package im.qxw.hello.spring.cloud.zuul.filter;
+
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+
+import java.io.IOException;
+
+@Component
+public class LoginFilter extends ZuulFilter {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoginFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext context = RequestContext.getCurrentContext();
+        HttpServletRequest request = context.getRequest();
+
+        logger.info("() >> {}", request.getMethod(), request.getRequestURL().toString());
+
+        String token = request.getParameter("token");
+        if (token == null) {
+            logger.warn("Token is empty");
+            context.setSendZuulResponse(false);
+            context.setResponseStatusCode(401);
+
+            try {
+                context.getResponse().getWriter().write("Token is empty");
+            } catch (IOException ex) {
+
+            }
+        } else {
+            logger.info("login succeed");
+        }
+
+        return null;
+    }
+}
